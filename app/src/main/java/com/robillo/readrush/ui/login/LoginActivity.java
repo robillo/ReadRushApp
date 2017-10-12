d5df542c69c281c1273ad45fb301b01d7cd7d083
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-10-12 21:35:38 +0530

    all perfectly done till login screen

--- app/src/main/java/com/robillo/readrush/ui/login/LoginActivity.java
+++ app/src/main/java/com/robillo/readrush/ui/login/LoginActivity.java
@@ -48,6 +48,14 @@ public class LoginActivity extends BaseActivity implements LoginMvpView {
     }
 
     @Override
+    public void onBackPressed() {
+        Intent a = new Intent(Intent.ACTION_MAIN);
+        a.addCategory(Intent.CATEGORY_HOME);
+        a.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(a);
+    }
+
+    @Override
     public void setLoginFragment() {
         FragmentManager manager = getSupportFragmentManager();
         manager.beginTransaction().replace(R.id.next, LoginFragment.newInstance()).commit();
