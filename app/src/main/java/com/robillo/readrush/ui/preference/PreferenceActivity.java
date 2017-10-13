483145671a9e662e6a201450765825100b9b33cb
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-10-13 21:04:23 +0530

    adapter inflated

--- app/src/main/java/com/robillo/readrush/ui/preference/PreferenceActivity.java
+++ app/src/main/java/com/robillo/readrush/ui/preference/PreferenceActivity.java
@@ -3,18 +3,31 @@ package com.robillo.readrush.ui.preference;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.robillo.readrush.R;
 import com.robillo.readrush.ui.base.BaseActivity;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import javax.inject.Inject;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class PreferenceActivity extends BaseActivity implements PreferenceMvpView {
 
+    PreferenceAdapter mAdapter;
+    List<String> mList = new ArrayList<>();
+
     @Inject
     PreferenceMvpPresenter<PreferenceMvpView> mPresenter;
+
+    @BindView(R.id.recycler)
+    RecyclerView mRecycler;
 
     public static Intent getStartIntent(Context context) {
         return new Intent(context, PreferenceActivity.class);
@@ -36,6 +49,10 @@ public class PreferenceActivity extends BaseActivity implements PreferenceMvpVie
 
     @Override
     protected void setUp() {
-
+        mList = Arrays.asList(getResources().getStringArray(R.array.preferences));
+        mAdapter = new PreferenceAdapter(mList, this);
+        GridLayoutManager manager = new GridLayoutManager(this, 3);
+        mRecycler.setLayoutManager(manager);
+        mRecycler.setAdapter(mAdapter);
     }
 }
