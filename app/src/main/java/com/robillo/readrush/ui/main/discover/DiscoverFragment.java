5e47fa3e2d1a5ad2a643cab978fd127424ec5531
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-10-19 21:39:36 +0530

    discover fragment ViewPager

--- app/src/main/java/com/robillo/readrush/ui/main/discover/DiscoverFragment.java
+++ app/src/main/java/com/robillo/readrush/ui/main/discover/DiscoverFragment.java
@@ -3,6 +3,10 @@ package com.robillo.readrush.ui.main.discover;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,16 +14,27 @@ import android.view.ViewGroup;
 import com.robillo.readrush.R;
 import com.robillo.readrush.di.component.ActivityComponent;
 import com.robillo.readrush.ui.base.BaseFragment;
+import com.robillo.readrush.ui.main.discover.PagerFragment.PagerFragment;
 import com.robillo.readrush.ui.main.library.LibraryFragment;
+import com.robillo.readrush.ui.onboard.OnboardActivity;
+import com.robillo.readrush.ui.onboard.fragment.OnboardFragment;
+import com.robillo.readrush.utils.page_transforms.CubeOutTransformer;
+import com.robillo.readrush.utils.page_transforms.ZoomOutPageTransformer;
 
 import javax.inject.Inject;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class DiscoverFragment extends BaseFragment implements DiscoverMvpView {
+
+    static int NUM_PAGES = 3;
+
+    @BindView(R.id.pager)
+    ViewPager mPager;
 
     @Inject
     DiscoverMvpPresenter<DiscoverMvpView> mPresenter;
@@ -56,5 +71,75 @@ public class DiscoverFragment extends BaseFragment implements DiscoverMvpView {
     @Override
     protected void setUp(View view) {
 
+        PagerAdapter adapter = new ScreenSlidePagerAdapter(getActivity().getSupportFragmentManager());
+        mPager.setAdapter(adapter);
+        mPager.setClipToPadding(false);
+        mPager.setPageTransformer(true, new ZoomOutPageTransformer());
+        mPager.addOnPageChangeListener(viewPagerPageChangeListener);
     }
+
+    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+        ScreenSlidePagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+
+            switch (position){
+                case 0:{
+
+                }
+                case 1:{
+
+                }
+                case 2:{
+
+                }
+                case 3:{
+
+                }
+                default:{
+
+                }
+            }
+            return new PagerFragment();
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_PAGES;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            return super.instantiateItem(container, position);
+        }
+
+        @Override
+        public float getPageWidth(int position) {
+            return 1.0f;
+        }
+
+    }
+
+    //  viewpager change listener
+    ViewPager.OnPageChangeListener viewPagerPageChangeListener = new ViewPager.OnPageChangeListener() {
+
+        @Override
+        public void onPageSelected(int position) {
+
+        }
+
+        @Override
+        public void onPageScrolled(int arg0, float arg1, int arg2) {
+
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int arg0) {
+
+        }
+    };
+
 }
