0090f9ea093cda5429710ecef7d85a417a21a618
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-10-22 19:35:47 +0530

    SearchActivity layout binding

--- app/src/main/java/com/robillo/readrush/ui/search/SearchActivity.java
+++ app/src/main/java/com/robillo/readrush/ui/search/SearchActivity.java
@@ -4,6 +4,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.robillo.readrush.R;
 import com.robillo.readrush.ui.base.BaseActivity;
@@ -11,9 +15,28 @@ import com.robillo.readrush.ui.main.MainActivity;
 
 import javax.inject.Inject;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class SearchActivity extends BaseActivity implements SearchMvpView {
+
+    @BindView(R.id.search)
+    EditText mSearchEditText;
+
+    @BindView(R.id.search_buttom)
+    ImageView mSearchButton;
+
+    @BindView(R.id.suggestions)
+    RecyclerView mSuggestionsRv;
+
+    @BindView(R.id.layout_suggestions)
+    LinearLayout mLayoutSuggestions;
+
+    @BindView(R.id.search_history)
+    RecyclerView mSearchHistory;
+
+    @BindView(R.id.layout_search_history)
+    LinearLayout mLayoutSearchHistory;
 
     @Inject
     SearchMvpPresenter<SearchMvpView> mPresenter;
