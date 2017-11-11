9e5d3e2260c334b6e40d8f2648a0fdf3493599a0
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-11-12 00:43:59 +0530

    retrofit post set user

---
+++ app/src/main/java/com/robillo/readrush/data/network/retrofit/ApiClient.java
@@ -0,0 +1,21 @@
+package com.robillo.readrush.data.network.retrofit;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by robinkamboj on 11/11/17.
+ */
+
+public class ApiClient {
+
+    public static final String BASE_URL = "http://readrush.in/index.php/";
+    private static Retrofit mRetrofitInstance;
+
+    public static Retrofit getClient(){
+        if(mRetrofitInstance == null){
+            mRetrofitInstance = new Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build();
+        }
+        return mRetrofitInstance;
+    }
+}
---
+++ app/src/main/java/com/robillo/readrush/data/network/retrofit/ApiInterface.java
@@ -0,0 +1,24 @@
+package com.robillo.readrush.data.network.retrofit;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
+/**
+ * Created by robinkamboj on 12/11/17.
+ */
+
+public interface ApiInterface {
+
+    @FormUrlEncoded
+    @POST("/new_api/create_user")
+    Call<ResponseBody> createUser(
+            @Field("name") String name,
+            @Field("email_id") String email,
+            @Field("password") String password,
+            @Field("preference") String preference
+    );
+
+}
