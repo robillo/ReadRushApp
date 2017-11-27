03f5be20ef2fd3e24c9cba570003b9d1e6a7372a
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-11-27 20:13:54 +0530

    top covers displayed

--- app/src/main/java/com/robillo/readrush/ui/main/discover/PagerFragment/PagerFragment.java
+++ app/src/main/java/com/robillo/readrush/ui/main/discover/PagerFragment/PagerFragment.java
@@ -9,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.robillo.readrush.R;
