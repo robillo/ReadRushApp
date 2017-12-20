9d55d50a5945c2ea7e5b424304caec4bcbdd1c15
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-12-20 19:06:30 +0530

    email help

--- app/src/main/java/com/robillo/readrush/ui/settings/SettingsActivity.java
+++ app/src/main/java/com/robillo/readrush/ui/settings/SettingsActivity.java
@@ -1,11 +1,15 @@
 package com.robillo.readrush.ui.settings;
 
+import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import com.robillo.readrush.R;
@@ -82,5 +86,26 @@ public class SettingsActivity extends BaseActivity implements SettingsMvpView {
     @OnClick(R.id.facebook)
     public void setmFacebook() {
         startActivity(WebViewActivity.getStartIntent(this, "Facebook", "https://www.facebook.com/readrush"));
+    }
+
+    @OnClick(R.id.membership_type)
+    public void setmMembershipType() {
+        Toast.makeText(this, "Normal Membership", Toast.LENGTH_SHORT).show();
+    }
+
+    @OnClick(R.id.email_help)
+    public void setmEmailHelp() {
+        String subject = "Help";
+        String bodyText = "ReadRush, ";
+
+        Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto","upendra@readrush.in", null));
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        emailIntent.putExtra(Intent.EXTRA_TEXT, bodyText);
+
+        try {
+            startActivity(Intent.createChooser(emailIntent, "Send email..."));
+        } catch (ActivityNotFoundException e) {
+            Log.e("EXCEPTION", "ACTIVITY NOT FOUND");
+        }
     }
 }
