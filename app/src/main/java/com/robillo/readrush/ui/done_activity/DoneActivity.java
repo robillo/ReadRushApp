e060d953aa28d530177812c12d0df02214259e54
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-12-24 18:09:23 +0530

    awesome material dialog for review and rating book

--- app/src/main/java/com/robillo/readrush/ui/done_activity/DoneActivity.java
+++ app/src/main/java/com/robillo/readrush/ui/done_activity/DoneActivity.java
@@ -15,12 +15,16 @@ import android.widget.TextView;
 import com.robillo.readrush.R;
 import com.robillo.readrush.ui.base.BaseActivity;
 import com.robillo.readrush.ui.rushread.ReadRushActivity;
+import com.stepstone.apprating.AppRatingDialog;
+import com.stepstone.apprating.listener.RatingDialogListener;
+
+import java.util.Arrays;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class DoneActivity extends BaseActivity {
+public class DoneActivity extends BaseActivity implements RatingDialogListener {
 
     @SuppressWarnings("FieldCanBeLocal")
     private static String mRushName = null;
@@ -65,10 +69,50 @@ public class DoneActivity extends BaseActivity {
 
         mRushName = getIntent().getStringExtra("rush_name");
         mName.setText(mRushName);
+
+        showDialog();
     }
 
     @OnClick(R.id.back)
     public void setmBack() {
         onBackPressed();
+    }
+
+    private void showDialog() {
+        new AppRatingDialog.Builder()
+                .setPositiveButtonText("Submit")
+                .setNegativeButtonText("Cancel")
+                .setNeutralButtonText("Later")
+                .setNoteDescriptions(Arrays.asList("Very Bad", "Not good", "Quite ok", "Very Good", "Excellent !!!"))
+                .setDefaultRating(2)
+                .setTitle("Rate this application")
+                .setDescription("Please select some stars and give your feedback")
+                .setDefaultComment("This book is..")
+                .setStarColor(R.color.dardRed)
+                .setNoteDescriptionTextColor(R.color.colorTextFour)
+                .setTitleTextColor(R.color.colorPrimary)
+                .setDescriptionTextColor(R.color.colorPrimaryDark)
+                .setHint("Please write your comment here ...")
+                .setHintTextColor(R.color.white)
+                .setCommentTextColor(R.color.white)
+                .setCommentBackgroundColor(R.color.dardReadBlack)
+                .setWindowAnimation(R.style.MyDialogFadeAnimation)
+                .create(DoneActivity.this)
+                .show();
+    }
+
+    @Override
+    public void onPositiveButtonClicked(int i, String s) {
+
+    }
+
+    @Override
+    public void onNegativeButtonClicked() {
+
+    }
+
+    @Override
+    public void onNeutralButtonClicked() {
+
     }
 }
